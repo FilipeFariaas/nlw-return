d79b405c97b05a91e6577e142a0a4a2aca74fb10
FilipeFariaas <filipefarias117@gmail.com>
2022-05-02 23:09:14 -0300

    add toggle content with blick function

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,9 +1,18 @@
 import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
 
 export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+
   return (
     <div className='absolute bottom-4 right-4'>
-      <button className='flex items-center bg-brand-500 rounded-full px-3 h-12 text-white group'>
+      {isWidgetOpen && <p>hello, world</p>}
+
+      <button onClick={toggleWidgetVisibility} className='flex items-center bg-brand-500 rounded-full px-3 h-12 text-white group'>
         <ChatTeardropDots className='w-6 h-6' />
 
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
